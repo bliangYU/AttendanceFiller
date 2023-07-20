19a31da9867739ca1ab5eb40661d7e65429bb751
bliangYU <128714733+bliangYU@users.noreply.github.com>
2023-07-20 14:36:56 -0400

    Add files via upload

---
+++ FormEntry.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+form_url = 'https://docs.google.com/forms/d/15XKq6Fzp5xM-j3lJBQ7eWmkTgpZCCUoIGjbjNAY_5cM/formResponse'
+
+response = requests.get(form_url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+email_input = soup.find('input', class_='Mh5jwe JqSWld yqQS1')
+
+data = {
+    'entry.1475566174': 'bliang112@gmail.com',
+    'entry.1684129569': 'Bill',
+    'entry.157508738': 'Liang',
+}
+
+response = requests.post(form_url, data=data)
+
+if response.status_code == 200:
+    print("Form submitted successfully.")
+else:
+    print("Failed to submit the form.")
